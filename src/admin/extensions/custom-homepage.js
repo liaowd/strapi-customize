a2c9ce44124b21f8be824b63574d964c7d3d7081
liaowd <794119927@qq.com>
2024-10-09 23:01:19 +0800

    remove some dom at admin welcome page that does not needed.

--- src/admin/extensions/custom-homepage.js
+++ src/admin/extensions/custom-homepage.js
@@ -1,1 +1,29 @@
 console.log('okok it\'s working')
+
+whenMutation(() => {
+  // remove some dom at admin welcome page that does not needed.
+  if(location.pathname==='/admin'){
+    let eles = document.querySelectorAll('a[href="https://strapi.io/blog"], .sc-blHHSb.fTSiDe.sc-ieEEOL.llYbcI')
+    eles.forEach(el=>el.remove())
+  }
+})
+
+function whenMutation(cb) {
+  const targetNode = document.getElementById("strapi");
+  if (!targetNode) return
+  const config = { attributes: false, childList: true, subtree: true };
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        cb()
+        console.log("A child node has been added or removed.");
+      } else if (mutation.type === "attributes") {
+        console.log(`The ${mutation.attributeName} attribute was modified.`);
+      }
+    }
+  };
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+  
+  // observer.disconnect();
+}
